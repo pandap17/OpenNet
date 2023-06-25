d19095af96021949570ee8fd99f6184fab19d63a
nezumouse <pinchese@protonmail.com>
2023-06-25 04:04:43 -0500

    penny's homepage

---
+++ sites/nezumi.qi/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    changeText();
+});
+
+function changeText() {
+    document.getElementById('textInput').value = 'it\'s raining furiously outside. now more than ever.';
+}
